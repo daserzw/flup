e4c458124abbff8958f7a93296e85680db70ee0b
Davide Vaghetti <davide.vaghetti@gmail.com>
2017-11-25 10:46:10 +0100

    Created config-template.py - switched from config_prod to standard config

--- flup/__init__.py
+++ flup/__init__.py
@@ -13,7 +13,7 @@ log_formatter = logging.Formatter(
 )
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-app.config.from_object('flup.config_prod')
+app.config.from_object('flup.config')
 #app.config.from_envvar('FLUP_SETTINGS')
 app.secret_key = app.config['SECRET_KEY']
 babel = Babel(app)
